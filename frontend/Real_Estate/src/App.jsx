814d0f7a4154b040529483e15e54ce473a8279ad
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2024-10-17 17:34:00 +0500

    Install Reactjs, TailwindCSS and create our React project

--- frontend/Real_Estate/src/App.jsx
+++ frontend/Real_Estate/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
+    <div className='text-red-700'>
+      App
+    </div>
   )
 }
