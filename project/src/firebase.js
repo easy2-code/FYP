85e9071be1830caafdf3829afc05f8de64a2fa20
Usman Habib <usmanhabib802@gmail.com>
2025-01-03 22:33:34 +0500

    Complete image upload functionality

--- project/src/firebase.js
+++ project/src/firebase.js
@@ -1,14 +1,32 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  authDomain: "mern-estate-7e895.firebaseapp.com",
+  projectId: "mern-estate-7e895",
+  storageBucket: "mern-estate-7e895.appspot.com",
+  messagingSenderId: "941477091473",
+  appId: "1:941477091473:web:6d22a229aeb154b4874730",
 };
 
 // Initialize Firebase
