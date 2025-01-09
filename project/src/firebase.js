0bd5cf4baddc1907085944f74d4f87c82c7a3e7f
easy2-code <worldferoze@gmail.com>
2025-01-09 16:58:44 +0500

    Protect firebase credentials.....

--- project/src/firebase.js
+++ project/src/firebase.js
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using Vite environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-7e895.firebaseapp.com",
-  projectId: "mern-estate-7e895",
-  storageBucket: "mern-estate-7e895.appspot.com",
-  messagingSenderId: "941477091473",
-  appId: "1:941477091473:web:6d22a229aeb154b4874730",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
